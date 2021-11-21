ddafbaa34a063f4524c63e746292c8abf5d8a111
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-11-21 23:23:15 +0700

    add schedule

--- app/src/main/java/com/android/pusatdatadprri/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/pusatdatadprri/ui/home/HomeFragment.kt
@@ -18,6 +18,12 @@ import com.android.pusatdatadprri.model.kategori.KategoriModel
 import com.android.pusatdatadprri.model.news.NewsModel
 import com.android.pusatdatadprri.ui.adapter.KategoriAdapter
 import com.android.pusatdatadprri.ui.adapter.NewsAdapter
+import com.android.pusatdatadprri.ui.home.adapter.CalendarAdapter
+import com.android.pusatdatadprri.ui.home.model.CalendarModel
+import com.github.dewinjm.monthyearpicker.MonthYearPickerDialogFragment
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class HomeFragment : Fragment() {
 
@@ -34,6 +40,14 @@ class HomeFragment : Fragment() {
     lateinit var gambarNews: TypedArray
     lateinit var idNews:Array<String>
     var news = arrayListOf<NewsModel>()
+
+    //calender
+    private val DAYS_COUNT = 42
+    private val calendarList = ArrayList<CalendarModel>()
+    private val calendar = Calendar.getInstance()
+    private var tahun : Int = -1
+    private var monthOfYear : Int = -1
+    private var adapter : CalendarAdapter = CalendarAdapter(calendarList)
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -55,6 +69,9 @@ class HomeFragment : Fragment() {
         bindDataKategori()
         bindDataNews()
 
+        loadCalendar()
+        aksiTombol()
+        bindCalender()
 
     }
 
@@ -98,5 +115,91 @@ class HomeFragment : Fragment() {
             layoutManager = gridLayoutManager
             adapter = adapterNews
         }
+    }
+
+    private fun loadCalendar() {
+        //ubah val ke var
+        val cells = ArrayList<CalendarModel>()         // inisialisasi variabel untuk setiap tanggal kalender
+        if (tahun != -1 && monthOfYear != -1 ){     // pengecekan bila varuiabel tahun dan monthOfYear kosong (-1 hanya pengecoh)
+            //ubah obyek kalender ke tahun dan bulan yang diterima
+            calendar.set(Calendar.MONTH, monthOfYear)
+            calendar.set(Calendar.YEAR, tahun)
+        } else {
+            // set variabel tahun dan monthOfYear ke tahun dan bulan sekarang
+            tahun = calendar.get(Calendar.YEAR)
+            monthOfYear = calendar.get(Calendar.MONTH)
+        }
+        var sdf = SimpleDateFormat("MMMM,yyyy", Locale("in", "ID"))  // obyek untuk parse bulan dan tahun
+        val dateToday = sdf.format(calendar.time).split(",") //format obyek calendar lalu split berdasarkan ,
+        binding.month.text = dateToday[0] //settext bulan ke textview month
+        binding.year.text = dateToday[1] //settext bulan ke textview year
+
+        //calendarToday
+        val calendarCompare : Calendar = Calendar.getInstance() //instansiasi obyek calendar pembanding
+
+        calendarCompare.set(Calendar.MONTH, monthOfYear) //set bulan pada calendar pembanding ke monthOfYear
+        calendarCompare.set(Calendar.YEAR, tahun) //set tahun pada calendar pembanding ke tahun
+
+
+        // memnentukan kapan tanggal dimulai pada bulan
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+        val monthBeginningCell = calendar.get(Calendar.DAY_OF_WEEK) - 1
+
+        // pindah calendar ke awal minggu
+        calendar.add(Calendar.DAY_OF_MONTH,-monthBeginningCell)
+
+        //obyek untuk parse tanggal
+        sdf = SimpleDateFormat("dd-MM-yyyy", Locale("in", "ID"))
+
+        // isi tanggal
+        while (cells.size < DAYS_COUNT) {
+            if(sdf.format(calendar.time).equals("22-11-2021")){
+                cells.add(CalendarModel(
+                    calendar.get(Calendar.DATE),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.YEAR),
+                    calendarCompare,
+                    "hijau"
+                ))
+            }else{
+                cells.add(CalendarModel(
+                    calendar.get(Calendar.DATE),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.YEAR),
+                    calendarCompare,
+                    null
+                ))
+            }
+            calendar.add(Calendar.DAY_OF_MONTH, 1)
+        }
+
+        calendarList.clear()
+        calendarList.addAll(cells)
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun aksiTombol() {
+        binding.month.setOnClickListener {
+            showMonthYearPicker()
+        }
+        binding.year.setOnClickListener {
+            showMonthYearPicker()
+        }
+    }
+
+    private fun showMonthYearPicker(){
+        val dialogFragment  = MonthYearPickerDialogFragment.getInstance(calendar.get(Calendar.MONTH), calendar.get(Calendar.YEAR))
+        dialogFragment.show(childFragmentManager, null)
+
+        dialogFragment.setOnDateSetListener { year, month ->
+            tahun = year
+            monthOfYear = month
+            loadCalendar()
+        }
+    }
+
+    private fun bindCalender() {
+        binding.recyclerView.layoutManager = GridLayoutManager(context, 7)
+        binding.recyclerView.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/android/pusatdatadprri/ui/home/model/CalendarModel.kt
@@ -0,0 +1,11 @@
+package com.android.pusatdatadprri.ui.home.model
+
+import java.util.*
+
+class CalendarModel(
+    var date: Int,
+    var month: Int,
+    var year : Int,
+    var calendarCompare : Calendar,
+    var status : String?
+)
